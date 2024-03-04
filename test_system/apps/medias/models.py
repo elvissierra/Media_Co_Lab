c60b3af59dc514324860f29b1f21546c177e881b
elvissierra <elvissierra3@gmail.com>
2024-03-04 13:35:43 -0600

    local files upload location

--- test_system/apps/medias/models.py
+++ test_system/apps/medias/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from test_system.apps.labels.models import Label
 from test_system.apps.users.models import User
 import uuid
 
@@ -7,6 +6,6 @@ import uuid
 class Medias(models.Model):
     title = models.CharField(max_length=255)
     id = models.UUIDField(default= uuid.uuid4, primary_key=True)
+    file = models.FileField(upload_to='static_files/')
     description = models.CharField(max_length=255)
     user = models.ForeignKey(User, related_name="medias", on_delete=models.CASCADE)
-
