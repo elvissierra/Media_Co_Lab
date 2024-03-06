70e9b955d077b7dfc5fc4571ec8dc0056a61224b
elvissierra <elvissierra3@gmail.com>
2024-03-06 14:07:40 -0600

    permissions

--- test_system/permissions.py
+++ test_system/permissions.py
@@ -1,14 +1,7 @@
 from rest_framework.permissions import BasePermission
 from datetime import datetime
 
-#allowed to create
-
-#allowed to label
-
-#allowed to delete/remove
-
 class OrganizationPermission(BasePermission):
-    #check organization
     def has_permission(self, request, view):
         org_id = request.parser_context.get("kwargs", {}).get("organization_id")
         return request.user.user.organization.id == org_id
@@ -19,8 +12,4 @@ class TeamPermission(BasePermission):
         team_id = request.parser_context.get("kwargs", {}).get("team_id")
         return request.user.user.team.id == team_id
     
-class CollaborationAllowance(BasePermission):
-
-    def has_permission(self, request, view):
-        #allowance for collaboration
-        #so if meeting say an hr then allowance can be given of an hr
+        
