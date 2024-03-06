46841c82d9976f21ae2d59e3450e21beadbb3233
elvissierra <elvissierra3@gmail.com>
2024-03-06 10:11:12 -0600

    permissions outline

--- test_system/permissions.py
+++ test_system/permissions.py
@@ -8,13 +8,13 @@ from datetime import datetime
 #allowed to delete/remove
 
 class OrganizationPermission(BasePermission):
-
+    #check organization
     def has_permission(self, request, view):
         org_id = request.parser_context.get("kwargs", {}).get("organization_id")
         return request.user.user.organization.id == org_id
     
 class TeamPermission(BasePermission):
-
+    #check team and allowance
     def has_permission(self, request, view):
         team_id = request.parser_context.get("kwargs", {}).get("team_id")
         return request.user.user.team.id == team_id
@@ -22,4 +22,5 @@ class TeamPermission(BasePermission):
 class CollaborationAllowance(BasePermission):
 
     def has_permission(self, request, view):
-        
+        #allowance for collaboration
+        #so if meeting say an hr then allowance can be given of an hr
