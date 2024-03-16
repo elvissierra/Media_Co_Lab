70cb22ce48b65beffde0457bd3e07160fc210559
elvissierra <elvissierra3@gmail.com>
2024-03-15 19:41:34 -0500

    auth

--- test_system/apis/users/views.py
+++ test_system/apis/users/views.py
@@ -5,6 +5,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from test_system.permissions import ObjectPermission
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import logout
 
 
 class UsersGetCreateView(APIView):
