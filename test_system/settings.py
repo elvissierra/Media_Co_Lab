cde8c246d58e1ebe12dc776d539ed48a7708ee8c
elvissierra <elvissierra3@gmail.com>
2024-03-18 18:40:42 -0500

    auth

--- test_system/settings.py
+++ test_system/settings.py
@@ -86,6 +86,8 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES":[
         "rest_framework.authentication.TokenAuthentication",
     ],
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",
+    ),
 }
 
 # Database
