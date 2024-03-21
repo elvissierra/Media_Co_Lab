d77774b920de50f095827677a7b29ca4ba3a163d
elvissierra <elvissierra3@gmail.com>
2024-03-21 17:21:39 -0500

    user identifier

--- test_system/apps/users/models.py
+++ test_system/apps/users/models.py
@@ -6,19 +6,22 @@ from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 
 class UserManager(BaseUserManager):
-    def create_user(self,  email, password=None, **extra_fields):
+    def create_user(self, username, email, password=None, **extra_fields):
         if not email:
-            raise ValueError("Email not provided properly.")
+            raise ValueError("The email must be provided.")
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
-        user.save(using=self._db)
+        user.save(using=self.db)
         return user
 
-    def create_superuser(self, email, password, **extra_fields):
+    def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
-
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Staff must be set.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must be set.")
         return self.create_user(email, password, **extra_fields)
 
 class CustomUser(AbstractUser):
@@ -26,15 +29,10 @@ class CustomUser(AbstractUser):
     last_name = models.CharField(max_length=255)
     id = models.UUIDField(default=uuid.uuid4, primary_key=True)
     team = models.ForeignKey(Team, related_name="users", on_delete=models.SET_NULL, blank=True, null=True)
-    organization = models.ForeignKey(
-        Organization,
-        related_name="users",
-        on_delete=models.CASCADE,
-        null=True,
-        blank=True,
-    )
-    objects= UserManager()
+    organization = models.ForeignKey( Organization, related_name="users", on_delete=models.CASCADE, null=True, blank=True,)
     email = models.EmailField(unique=True)
+
+    objects= UserManager()
 
     def __str__(self):
         return self.email
