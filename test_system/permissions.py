4e741e59c77518834b0fa8e298f0320c27e7a871
elvissierra <elvissierra3@gmail.com>
2024-04-08 11:17:39 -0500

    user req pars

--- test_system/permissions.py
+++ test_system/permissions.py
@@ -1,18 +1,18 @@
 from rest_framework.permissions import BasePermission
-from datetime import datetime
+
 
 class OrganizationPermission(BasePermission):
     def has_permission(self, request, view):
         org_id = request.parser_context.get("kwargs", {}).get("organization_id")
-        return request.user.user.organization.id == org_id
+        return request.user.organization.id == org_id
     
 class TeamPermission(BasePermission):
     def has_permission(self, request, view):
         team_id = request.parser_context.get("kwargs", {}).get("team_id")
-        return request.user.user.team.id == team_id
+        return request.user.team.id == team_id
     
 class ObjectPermission(BasePermission):
     def is_owner(self, request, view):
         user_id = request.parser_context.get("kwargs", {}).get("user_id")
-        return request.user.user.id == user_id
+        return request.user.id == user_id
     
