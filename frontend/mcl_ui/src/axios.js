412651ce749e540fb22a77e5da186fcbe20938b3
elvissierra <elvissierra3@gmail.com>
2024-08-26 12:46:10 -0500

    axios

---
+++ frontend/mcl_ui/src/axios.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import router from './router'; // Adjust the path to your router file
+
+// Create an Axios instance
+const axiosInstance = axios.create({
+  baseURL: 'http://localhost:8000/api/',  // Replace with your API base URL
+});
+
+// Request interceptor to add the Authorization header
+axiosInstance.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem('authToken');
+    if (token) {
+      config.headers['Authorization'] = `Token ${token}`;
+    }
+    return config;
+  },
+  error => Promise.reject(error)
+);
+
+// Response interceptor to handle 401 Unauthorized
+axiosInstance.interceptors.response.use(
+  response => response,
+  error => {
+    if (error.response && error.response.status === 401) {
+      // If 401 Unauthorized, log out the user
+      localStorage.removeItem('authToken');
+      router.push({ name: 'UserLogin' });  // Redirect to the login page
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
--- frontend/mcl_ui/src/main.js
+++ frontend/mcl_ui/src/main.js
@@ -2,11 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
+import axiosInstance from './axios'
 
 const app = createApp(App)
 
+// backend connection
 axios.defaults.baseURL = 'http://localhost:8000/api/'
-app.config.globalProperties.$axios = axios
+app.config.globalProperties.$axios = axiosInstance
 
 app.use(router)
 
