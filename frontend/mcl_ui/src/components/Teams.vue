c406385b319a86fe458e666eb9dce682bf30a5d8
elvissierra <elvissierra3@gmail.com>
2024-08-27 11:23:34 -0500

    vuetify

--- frontend/mcl_ui/src/components/Teams.vue
+++ frontend/mcl_ui/src/components/Teams.vue
@@ -1,35 +1,51 @@
 <template>
-  <div>
-    <h1>Teams</h1>
-    <ul>
-      <li v-for="team in teams" :key="team.id">
-        <h2>{{ team.title }}</h2>
-        <p>{{ team.description }}</p>
-        <ul>
-          <li v-for="user in team.users" :key="user.id">
-            {{ user.first_name }}
-          </li>
-        </ul>
-      </li>
-    </ul>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <h1 class="text-center">Teams</h1>
+      </v-col>
+    </v-row>
+    
+    <v-row>
+      <v-col
+        v-for="team in teams"
+        :key="team.id"
+        cols="12"
+        sm="6"
+        md="4"
+      >
+        <v-card class="mb-4">
+          <v-card-title>
+            {{ team.title }}
+          </v-card-title>
+          <v-card-text>
+            {{ team.description }}
+          </v-card-text>
+          <v-card-actions>
+            <v-btn color="primary">View Team</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: 'mclTeams',
+  name: 'mclTeam',
   data() {
     return {
-      teams: [],  // Array to store the fetched teams
+      teams: [],
     };
   },
   async created() {
     try {
-      const response = await this.$axios.get('/teams/');  // Use this.$axios
+      const response = await this.$axios.get('/teams/');
       this.teams = response.data;
     } catch (error) {
       console.error('Error fetching teams:', error);
     }
-  }
+  },
 };
 </script>
+
--- frontend/mcl_ui/src/main.js
+++ frontend/mcl_ui/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 import axiosInstance from './axios'
+import vuetify from './plugins/vuetify'
 
 const app = createApp(App)
 
@@ -10,6 +11,7 @@ const app = createApp(App)
 axios.defaults.baseURL = 'http://localhost:8000/api/'
 app.config.globalProperties.$axios = axiosInstance
 
+app.use(vuetify)
 app.use(router)
 
 app.mount('#app')
