7f2c495acb5e73626ea1fadb7ef1ad8e0b7a0a30
elvissierra <elvissierra3@gmail.com>
2024-08-28 12:23:12 -0500

    team detail vue

---
+++ frontend/mcl_ui/src/components/TeamDetail.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <h1 class="text-center">{{ team.title }}</h1>
+        <p>{{ team.description }}</p>
+        <!-- Add more team details as needed -->
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'TeamDetail',
+  data() {
+    return {
+      team: null,
+    }
+  },
+  async created() {
+    const teamId = this.$route.params.id
+    try {
+      const response = await this.$axios.get(`/teams/${teamId}/`)
+      this.team = response.data
+    } catch (error) {
+      console.error('Error fetching team details:', error)
+    }
+  },
+}
+</script>
