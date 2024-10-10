6475413c0fd1cb4622adcfa39b1b216f8ddcad3b
elvissierra <elvissierra3@gmail.com>
2024-10-09 21:21:21 -0500

    team vue refactor

--- frontend/mcl_ui/src/components/Teams.vue
+++ frontend/mcl_ui/src/components/Teams.vue
@@ -1,14 +1,19 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col cols="12">
-        <h1 class="text-center">Teams</h1>
+  <v-container class="pa-4">
+    <v-row justify="center">
+      <v-col cols="12" md="8" class="text-center">
+        <h1 class="mb-5">Teams</h1>
       </v-col>
     </v-row>
     
-    <v-alert v-if="error" type="error">
-    {{ error }}
-    </v-alert>
+    <v-row justify="center">
+      <v-col cols="12" md="8">
+        <v-alert v-if="error" type="error" dismissible>
+          {{ error }}
+        </v-alert>
+      </v-col>
+    </v-row>
+    
     <v-row>
       <v-col
         v-for="team in teams"
@@ -22,23 +27,31 @@
           @click="viewTeam(team.id)"
           outlined
           hover
+          elevation="2"
         >
+          <v-img
+            :src="team.image || 'https://via.placeholder.com/150'"
+            height="150px"
+          ></v-img>
           <v-card-title>
-            {{ team.title }}
+            <div>
+              <h3 class="headline mb-1">{{ team.title }}</h3>
+              <p class="grey--text">{{ team.description.length > 100 ? team.description.substring(0, 100) + '...' : team.description }}</p>
+            </div>
           </v-card-title>
+          <v-divider></v-divider>
           <v-card-text>
-            <p>{{ team.description }}</p>
-            <v-divider></v-divider>
-            <v-list dense>
-              <v-list-item
+            <v-chip-group column>
+              <v-chip
                 v-for="user in team.users"
                 :key="user.id"
+                class="ma-1"
+                outlined
+                color="primary"
               >
-                <v-list-item-content>
-                  {{ user.first_name }}
-                </v-list-item-content>
-              </v-list-item>
-            </v-list>
+                {{ user.first_name }}
+              </v-chip>
+            </v-chip-group>
           </v-card-text>
         </v-card>
       </v-col>
