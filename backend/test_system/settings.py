7efbd997c7375bf5eca3e7c41faa66d8fda903d1
elvissierra <elvissierra3@gmail.com>
2024-10-28 14:45:01 -0500

    format settings

--- backend/test_system/settings.py
+++ backend/test_system/settings.py
@@ -3,11 +3,10 @@ import environ
 import os
 
 env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 
 # Quick-start development settings - unsuitable for production
