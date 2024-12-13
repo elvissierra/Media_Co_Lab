e6e241f0f37eae4cc272d7f97297faaf41282118
elvissierra <elvissierra3@gmail.com>
2024-12-13 10:33:51 -0600

    combined TP and NB

--- frontend/mcl_ui/src/App.vue
+++ frontend/mcl_ui/src/App.vue
@@ -60,14 +60,6 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
 .top_page {
   text-align: center;
