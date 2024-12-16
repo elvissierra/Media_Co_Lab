8fc822bab0be367f133486e2e36dfa7a81bc8306
elvissierra <elvissierra3@gmail.com>
2024-12-15 22:57:47 -0600

    resource sharing

--- backend/test_system/settings.py
+++ backend/test_system/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -41,11 +42,10 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8080",
+    os.getenv("CORS_ALLOWED_ORIGINS"),
 ]
 
 ROOT_URLCONF = "test_system.urls"
