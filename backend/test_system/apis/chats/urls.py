2ce61777aaa0c5925ac5e3f448dbdaf92aecf0f0
elvissierra <elvissierra3@gmail.com>
2025-02-04 23:08:45 -0600

    app rename

--- backend/test_system/apis/chats/urls.py
+++ backend/test_system/apis/chats/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.ChatsGetView.as_view(), name="ChatsGetOrCreate"),
-    path("<uuid:Chat_id>/", views.ChatGetUpdateDeleteView.as_view(), name="ChatsGetUpdateDelete"),
+    path("<uuid:chat_id>/", views.ChatGetUpdateDeleteView.as_view(), name="ChatGetUpdateDelete"),
 ]
--- backend/test_system/urls.py
+++ backend/test_system/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     path("api/organizations/", include("test_system.apis.organizations.urls")),
     path("api/labels/", include("test_system.apis.labels.urls")),
     path("api/medias/", include("test_system.apis.medias.urls")),
-    path("api/comments/", include("test_system.apis.comments.urls")),
+    path("api/chats/", include("test_system.apis.chats.urls")),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
