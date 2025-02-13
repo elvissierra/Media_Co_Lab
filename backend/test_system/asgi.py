dcd95b4edddf85f8273e29cd10f2abd076f013d8
elvissierra <elvissierra3@gmail.com>
2025-02-12 22:15:24 -0600

    chats routing

--- backend/test_system/asgi.py
+++ backend/test_system/asgi.py
@@ -1,21 +1,21 @@
-"""
-ASGI config for test_system project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
-
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_system.settings')
 
+django_asgi_app = get_asgi_application()
+
+from test_system.apis.chats.routing import websocket_urlpatterns
+
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application()
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        )
     }
 )
